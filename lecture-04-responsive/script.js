4852732a5253d93ce2fcf0887a1bd85051afc3fb
Joni Dunkel <jorodunk@gmail.com>
2026-02-05 14:35:16 +0200

    Lecture 5 - Fixed bug with darkmode initial state

--- lecture-04-responsive/script.js
+++ lecture-04-responsive/script.js
@@ -1,12 +1,15 @@
 console.log("Page loaded");
-
 let clickCount = 0;
 
 onclick = function () {
     clickCount++;
     console.log("Mouse clicked " + clickCount + " times");
 }
+
 let darkmode = false;
+document.body.classList.toggle("darkmode", darkmode);
+document.body.classList.toggle("lightmode", !darkmode);
+
 const darkmodeBtn = document.getElementById("darkModeToggle");
 darkmodeBtn.onclick = function () {
     darkmode = !darkmode;
